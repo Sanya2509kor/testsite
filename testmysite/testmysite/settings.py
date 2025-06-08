a4a9daf7fc374900e1225892aa3f1e8923857422
Sanya2509kor <korchemkinaleksandr@yandex.ru>
2025-06-08 20:48:39 +0700

    добавил debug toolbar

--- testmysite/testmysite/settings.py
+++ testmysite/testmysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "debug_toolbar",
     
     'main',
     'goods',
@@ -53,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
 ]
 
 ROOT_URLCONF = 'testmysite.urls'
@@ -125,6 +130,13 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- testmysite/testmysite/urls.py
+++ testmysite/testmysite/urls.py
@@ -18,10 +18,18 @@ from argparse import Namespace
 from django.contrib import admin
 from django.urls import include, path
 
+from testmysite.settings import DEBUG
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('catalog/', include('goods.urls', namespace='catalog'))
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
+
+if DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
