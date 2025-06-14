21e780e84d20066f9e461c854fceaaecea332998
Sanya2509kor <korchemkinaleksandr@yandex.ru>
2025-06-14 14:36:02 +0700

    Изменение поиска на сайте

--- testmysite/testmysite/settings.py
+++ testmysite/testmysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     "debug_toolbar",
     
@@ -87,8 +88,12 @@ WSGI_APPLICATION = 'testmysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Korch2509!Sanya',
+        'HOST': 'guegietiti.beget.app',
+        'PORT': '5432',
     }
 }
 
